1c518d70f3e3ff7e50d2ac1b68f35d48dbccade5
leandro456 <jleandrodorado@gmail.com>
2016-09-13 02:50:55 -0400

    añadiendo reportes de vacaciones

---
+++ frontend/views/empleado/reportevacaciones.php
@@ -0,0 +1,15 @@
+<?php
+
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Viatico */
+/* @var $form ActiveForm */
+$this->title = "Reporte Vacaciones";
+/*$this->params['breadcrumbs'][] = ['label' => 'Empleados', 'url' => ['index']];*/
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="empleado-reportevacaciones">
+    <h2 align="center">Vacaciones</h2>
+    
+</div><!-- empleado-reportevacaciones -->
